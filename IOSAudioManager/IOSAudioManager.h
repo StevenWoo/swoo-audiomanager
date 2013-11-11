18cc1aaa6d25b9d324c88c9f456e550f108cafff
Steven Woo <swoo@tackable.com>
2013-11-11 12:42:16 -0800

    Initial Commit

---
+++ IOSAudioManager/IOSAudioManager.h
@@ -0,0 +1,13 @@
+//
+//  IOSAudioManager.h
+//  IOSAudioManager
+//
+//  Created by Steven Woo on 11/11/13.
+//  Copyright (c) 2013 Steven Woo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IOSAudioManager : NSObject
+
+@end
